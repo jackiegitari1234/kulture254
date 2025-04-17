d62e38a040c9c4f0fea5fe412f3e7e045950aad6
jackie <mgjackline@gmail.com>
2025-04-17 13:47:38 +0300

    error handling

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import productRoutes from './routes/productRoutes.js';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleWare,js';
+
 const port = process.env.PORT || 5000;
 connectDB();
 
@@ -14,5 +16,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
